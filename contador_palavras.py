d1e378751eb81f5c37484eaec5a67f94c78fec8c
Alisson Casagrande <contato@grande.app.br>
2026-03-16 15:22:38 -0300

    corpo do contator de palavaras, e alguns arquivos de texto

--- contador_palavras.py
+++ contador_palavras.py
@@ -3,5 +3,22 @@
 #e conte número de palavras presentes nela. O programa 
 #deve exibir o resultado para o usuário.
 
-pass
+while True:
+    try:
+        nome_arquivo = input("Digite o nome do arquivo de texto (com extensão .txt) (padrao: entrada.txt) (ou SAIR): ")
+        if nome_arquivo.upper() == "SAIR":
+            print("Encerrando o programa.")
+            break
+        if not nome_arquivo:
+            nome_arquivo = 'entrada.txt'
+
+        with open(nome_arquivo, 'r') as f:
+            texto = f.read()
+
+        palavras = texto.split()
+        print(f"O número de palavras no arquivo é: {len(palavras)}")
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+
+
 
